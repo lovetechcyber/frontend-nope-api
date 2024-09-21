244f35aa563c3ccbe83306402f4190872dd8ffb6
lovetechcyber <lovetechcyb@gmail.com>
2024-09-21 21:54:19 +0100

    Add .gitattributes to manage line endings

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -14,7 +14,7 @@ const Product = ({ product, getProducts }) => {
 
     if (result.isConfirmed) {
       try {
-        await axios.delete(`http://localhost:3000/api/products/${id}`);
+        await axios.delete(`https://node-api-sk05.onrender.com/api/products/${id}`);
         toast.success("Delete a Product Successfully");
         getProducts();
       } catch (error) {
